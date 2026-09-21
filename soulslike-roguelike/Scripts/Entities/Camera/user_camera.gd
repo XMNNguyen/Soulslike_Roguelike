2da5b845371f36e313e50cfc42732520c966f474
XMNNguyen <rupyman95@gmail.com>
2026-09-21 10:34:48 -0700

    added base camera class and user camera

--- soulslike-roguelike/Scripts/Entities/Camera/user_camera.gd
+++ soulslike-roguelike/Scripts/Entities/Camera/user_camera.gd
@@ -6,12 +6,17 @@ class_name UserCamera
 @onready var spring_arm : SpringArm3D = $Pivot/SpringArm3D
 @onready var camera : Camera3D = $Pivot/SpringArm3D/Camera3D
 
+@export var sensitivity : Float = 0.5
+@export var tilt_limit : Float = 90
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		spring_arm.rotation.x -= event.relative.y * sensitivity
+		spring_arm.rotation.y -= event.relative.x * sensitivity
+		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -deg_to_rad(tilt_limit), deg_to_rad(tilt_limit))
