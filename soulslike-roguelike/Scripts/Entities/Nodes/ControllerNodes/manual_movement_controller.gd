bde0cfadf97b8b35ed2a780c910e6bd8999686ca
XMNNguyen <rupyman95@gmail.com>
2026-09-22 11:53:41 -0700

    fixed player orientation while moving

--- soulslike-roguelike/Scripts/Entities/Nodes/ControllerNodes/manual_movement_controller.gd
+++ soulslike-roguelike/Scripts/Entities/Nodes/ControllerNodes/manual_movement_controller.gd
@@ -2,7 +2,7 @@ extends ControllerNode
 class_name InputMovementController
 
 @export var SPEED : float = 4.5
-@export var TURN_WEIGHT : float = 0.1
+@export var TURN_WEIGHT : float = 0.5
 @export var target : CharacterBody3D = null
 
 
@@ -12,26 +12,34 @@ func run(delta : float) -> void:
 		return
 	
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var direction := Vector3(input_dir.x, 0.0, input_dir.y)
 	
 	# make sure to rotate the basis in the correct direction if there is a camera
-	var camera : Camera3D = get_viewport().get_camera_3d()
-	var direction : Vector3 = Vector3.ZERO
+	var camera := get_viewport().get_camera_3d()
+
 	if camera:
-		var move_basis : Basis = camera.global_transform.basis
-		
-		move_basis.z.y = 0
-		move_basis.x.y = 0
-		
-		direction =  Vector3(input_dir.x, 0.0, input_dir.y) * move_basis.orthonormalized()
+		var forward := -camera.global_transform.basis.z # we use negative to have character facing away from camera
+		var right := camera.global_transform.basis.x
+
+		# Ignore camera pitch
+		forward.y = 0.0
+		right.y = 0.0
+
+		forward = forward.normalized()
+		right = right.normalized()
+
+		direction = right * input_dir.x + forward * -input_dir.y
 	else:
-		direction = (target.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		
-	# handle moving the player
+		direction = Vector3(input_dir.x, 0.0, input_dir.y)
+
 	if direction:
+		direction = direction.normalized()
+
 		target.velocity.x = direction.x * SPEED
 		target.velocity.z = direction.z * SPEED
-		var target_basis = Basis.looking_at(direction.normalized(), Vector3.UP)
-		target.basis = target.basis.slerp(target_basis, TURN_WEIGHT).orthonormalized()
+
+		var target_basis := Basis.looking_at(-direction, Vector3.UP)
+		target.basis = target.basis.slerp(target_basis, TURN_WEIGHT)
 	else:
-		target.velocity.x = move_toward(target.velocity.x, 0, SPEED)
-		target.velocity.z = move_toward(target.velocity.z, 0, SPEED)
+		target.velocity.x = move_toward(target.velocity.x, 0.0, SPEED)
+		target.velocity.z = move_toward(target.velocity.z, 0.0, SPEED)
